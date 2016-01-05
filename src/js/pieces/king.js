c2fdcbf46efe458e0fee2c5930ced1893294db19
Aditya Mishra <aditya.mishra@comprotechnologies.com>
2016-01-05 17:22:36 +0530

    defined legalMoves for king

--- src/js/pieces/king.js
+++ src/js/pieces/king.js
@@ -1,5 +1,30 @@
 function king(rank, file, side, type) {
 	return new piece(rank, file, side, type, function (board) {
 
+		var steps = [];
+		for (var i = -1; i < 2; i++)
+			for (var j = -1; j < 2; j++)
+				if (i !== 0 || j !== 0)
+					steps.push([i, j]);
+		for (var x = 0; x < steps.length; x++)
+			if (!board[this.rank + steps[x][0]][this.file + steps[x][1]].piece)
+				m.push({
+					rank: this.rank + steps[x][0],
+					file: this.file + steps[x][1]
+				});
+			else {
+				if (board[this.rank + steps[x][0]][this.file + steps[x][1]].piece.side !== this.side)
+					k.push({
+						rank: this.rank + steps[x][0],
+						file: this.file + steps[x][1]
+					});
+			}
+
+		return {
+			move: m,
+			capture: k,
+			castle: undefined
+		};
+
 	});
 }
