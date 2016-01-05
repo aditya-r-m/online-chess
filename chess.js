01e9435d08790a86fd980cf8456e92d2ba4a0281
Aditya Mishra <aditya.mishra@comprotechnologies.com>
2016-01-05 10:49:27 +0530

    added dependencies

--- chess.js
+++ chess.js
@@ -1,1 +1,16 @@
+var http = require("http");
+var express = require("express");
+var path = require("path");
+var app = express();
 
+
+app.use('/static', express.static(path.join(__dirname, '/public')));
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/home.html');
+	console.log("served file");
+});
+
+app.listen(3000, function () {
+	console.log("listening");
+});
---
+++ public/js/script.js
@@ -0,0 +1,1 @@
+console.log("successfulle loaded the script");
